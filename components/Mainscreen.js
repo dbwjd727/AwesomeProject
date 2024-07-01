6daf6e2eac1801ea36575555d05ea366f06dc689
kwon-yujung <yuj0727@gmail.com>
2024-07-02 08:28:27 +0900

    setting

---
+++ components/Mainscreen.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { StyleSheet, Platform } from 'react-native';
+
+function first() {
+
+}
+
+return (
+    <>
+        <div>
+            <p>hello world</p>
+        </div>
+    </>
+)
